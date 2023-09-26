ee8c3ea2f882fc1ef00b70a2811dab3c659363ab
gjlander <garrett.jlander@gmail.com>
2023-09-26 17:30:45 +0200

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // import { useState } from "react";
-
+import Navbar from "./Navbar";
 function App() {
     return (
         <>
+            <Navbar />
             <h1> Howdy folks, shall we copy Hacker News?</h1>
         </>
     );
